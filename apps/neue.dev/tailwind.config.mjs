9d5e5dbbce3ee07400904557e1a182c746033db6
Aaron Crockett <aaron.crockett.00@gmail.com>
2023-12-19 15:53:22 -0800

    wip: rename file

--- apps/neue.dev/tailwind.config.mjs
+++ apps/neue.dev/tailwind.config.mjs
@@ -1,6 +1,6 @@
 const plugin = require('tailwindcss/plugin')
 
-const { twColors, twPlugin } = require('../../packages/neue-css/scripts/generate-css')
+const { twColors, twPlugin } = require('../../packages/neue-css/scripts/generate')
 
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', '../../packages/neue-css/**/*.{js}'],
