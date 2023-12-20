7c88a3571fdf6f58b2cce0ebe85177a6edbea895
Aaron Crockett <aaron.crockett.00@gmail.com>
2023-12-19 20:15:18 -0800

    bug: remove accidental code reversion

---
+++ apps/neue.dev/src/components/theme-generator/partials/RoundedMaker.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+  import { getThemeOptionsStore } from '../data/stores'
+  import { roundedOpts, elementRoundness } from '../data/settings'
+  import { createEventDispatcher } from 'svelte'
+
+  const dispatch = createEventDispatcher()
+
+  const themeOptionsStore = getThemeOptionsStore() as any
+
+  function handleInputChange(event: any) {
+    const inputName = event?.target?.name
+    const inputValue = event?.target?.value
+
+    themeOptionsStore.updateRoundedOpts({ [inputName]: inputValue })
+    dispatch('roundedInputChange')
+  }
+</script>
+
+<!-- Add the select dropdown in your template -->
+<div class="space-y-4">
+  <label class="options-input-wrapper">
+    <span>Theme Roundness:</span>
+    <select
+      class="w-full my-select"
+      bind:value={$themeOptionsStore.roundedOpts.size}
+      on:change={(e) => handleInputChange(e)}
+    >
+      {#each Object.entries(roundedOpts) as [key, value]}
+        <option {value}>{key}</option>
+      {/each}
+    </select>
+  </label>
+  {#if $themeOptionsStore.roundedOpts.size !== '--ui-roundness-none'}
+    <label class="options-input-wrapper">
+      <span>Button Roundness:</span>
+      <select
+        class="w-full my-select"
+        bind:value={$themeOptionsStore.roundedOpts.btnRoundness}
+        on:change={(e) => handleInputChange(e)}
+      >
+        {#each Object.entries(elementRoundness) as [key, value]}
+          <option {value}>{key}</option>
+        {/each}
+      </select>
+    </label>
+  {/if}
+  {#if $themeOptionsStore.roundedOpts.size !== '--ui-roundness-none'}
+    <label class="options-input-wrapper">
+      <span>Input Roundness:</span>
+      <select
+        class="w-full my-select"
+        bind:value={$themeOptionsStore.roundedOpts.inputRoundness}
+        on:change={(e) => handleInputChange(e)}
+      >
+        {#each Object.entries(elementRoundness) as [key, value]}
+          <option {value}>{key}</option>
+        {/each}
+      </select>
+    </label>
+  {/if}
+</div>
