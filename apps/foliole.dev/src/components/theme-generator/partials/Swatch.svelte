178ebc0daa371ff4200b9abff8ccd835338e3627
Aaron Crockett <aaron.crockett.00@gmail.com>
2024-01-07 12:50:19 -0800

    #78 tests for swatch component

--- apps/foliole.dev/src/components/theme-generator/partials/Swatch.svelte
+++ apps/foliole.dev/src/components/theme-generator/partials/Swatch.svelte
@@ -9,15 +9,9 @@
 <div class="flex order-3 md:order-2">
   {#each Object.entries(steppedSwatchColorClasses[color]).filter( ([shade]) => stops.includes(shade), ) as [shade, shadeClasses]}
     <div class="grid grid-rows-[1fr_40px] text-center w-full">
-      <!-- Label -->
-      <div class="text-sm" />
-      <!-- Swatch -->
-
-      <div class="h-full flex justify-center items-center {shadeClasses}">
+      <div class="h-full flex justify-center items-center {shadeClasses}" data-testid="swatch">
         {#if shade === 'base'}
-          <div>
-            <span class="hidden sm:inline-block">Text</span>
-          </div>
+          <span class="hidden sm:inline-block">Text</span>
         {/if}
       </div>
     </div>
--- apps/foliole.dev/src/components/theme-generator/partials/Swatch.test.ts
+++ apps/foliole.dev/src/components/theme-generator/partials/Swatch.test.ts
@@ -1,7 +1,34 @@
 import { test, expect } from 'vitest'
 import { render } from '@testing-library/svelte'
+import '@testing-library/jest-dom'
+
 import Swatch from './Swatch.svelte'
 
 test('Swatch renders without throwing', () => {
   expect(() => render(Swatch)).not.toThrow()
+})
+
+test('renders the correct number of swatches', () => {
+  const { container } = render(Swatch, { stops: ['mlt', 'base', 'mdk'] })
+  expect(container.querySelectorAll('.grid').length).toBe(3)
+})
+
+test('renders the "Text" label for the "base" swatch', () => {
+  const { getByText } = render(Swatch, { stops: ['base'] })
+  expect(getByText('Text')).toBeInTheDocument()
+})
+
+test('applies the correct color classes to the swatches', () => {
+  const { getByTestId } = render(Swatch, { color: 'primary', stops: ['base'] })
+  const swatch = getByTestId('swatch')
+  expect(swatch.className).toContain('bg-primary')
+})
+
+test('applies the correct color classes to the swatches', () => {
+  const { queryAllByTestId } = render(Swatch, { color: 'primary', stops: ['mlt', 'base', 'mdk'] })
+  const swatches = queryAllByTestId('swatch')
+
+  expect(swatches[0].className.split(' ')).toContain('bg-primary-mlt')
+  expect(swatches[1].className.split(' ')).toContain('bg-primary')
+  expect(swatches[2].className.split(' ')).toContain('bg-primary-mdk')
 })
